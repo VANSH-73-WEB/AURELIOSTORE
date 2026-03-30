b27ba73a62f8af5bcdfad4fe0ba4457643839a37
vansh parashar <v333777abc@gmail.com>
2026-03-30 23:45:56 +0530

    done

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {protect} from "../middleware/authmiddleware.js";
+import {protect} from "../middleware/authMiddleware.js";
 import { getMyOrders } from "../controllers/orderController.js";
 const router = express.Router();
 
