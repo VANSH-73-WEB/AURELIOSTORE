f9a971eeac173ceed864fba3326e2f25221674dd
vansh parashar <v333777abc@gmail.com>
2026-04-01 12:36:43 +0530

    done

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {protect} from "./middleware/authMiddleware.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 import { addToCart, getCart, increaseQty, decreaseQty, removeFromCart } from "../controllers/cartcontroller.js";
 
