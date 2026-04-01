d182d6f78811f3463915b06fba540bb072e96749
vansh parashar <v333777abc@gmail.com>
2026-04-01 12:04:56 +0530

    commit

--- backend/middleware/authmiddleware.js
+++ backend/middleware/authmiddleware.js
@@ -16,7 +16,10 @@ export const protect = async (req, res, next) => {
     } else {
       return res.status(401).json({ message: "Not authorized, no token" });
     }
-  } catch (error) {
+
+  } 
+  
+  catch (error) {
     return res.status(401).json({ message: "Token failed" });
   }
 };
